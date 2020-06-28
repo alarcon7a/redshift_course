34d24b6ca49ef7091fdb231b54a26832d2cf8722
alarcon7a <alarcon7a@gmail.com>
2020-06-28 14:54:53 -0500

    [evaluar_sort] evaluar el ordenamiento mediante llaves

--- Claves_de_ordenamiento/create_table_ordenamiento.sql
+++ Claves_de_ordenamiento/create_table_ordenamiento.sql
@@ -5,6 +5,11 @@ CREATE TABLE public.cust_sales_date (
 	c_mktsegment varchar(10) NULL,
 	d_date date NULL,
 	lo_revenue int4 NULL
-)
+);
+
+copy cust_sales_date from 's3://<Reemplazar_con_bucket>/cust_sales_date_000.bz2' 
+credentials 'aws_iam_role=<Reemplazar_con_tu_iam_role_arn>' 
+BZIP2
+region 'us-east-2';
 
 
