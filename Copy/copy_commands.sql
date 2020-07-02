b077fb672f26d8b91692e8457c8d1f6d07be11ac
alarcon7a <alarcon7a@gmail.com>
2020-07-02 16:18:36 -0500

    [Copy] add copy commans for load data on redshift

---
+++ Copy/copy_commands.sql
@@ -0,0 +1,41 @@
+--Create table
+create table estudiante 
+( id int2, 
+nombre varchar(20),
+apellido varchar(20),
+edad int2, 
+fecha_ingreso date );
+
+--Cargue con banner y delimitado por ; (Fallan los dos primeros)
+copy estudiante from '<Ruta del archivo>' 
+credentials 'aws_iam_role=<I am ARN>' 
+region '<Region>'
+delimiter ';'-- sin delimiter primero
+ignoreheader 1 -- sin header primero
+IGNOREBLANKLINES -- sin IGNOREBLANKLINES primer
+BLANKSASNULL -- sin IGNOREBLANKLINES primer
+;
+
+--Cargue con banner y delimitado por ; con otro formato
+copy estudiante from '<Ruta del archivo>' 
+credentials 'aws_iam_role=<I am ARN>' 
+region '<Region>'
+delimiter ';'
+ignoreheader 1 
+IGNOREBLANKLINES 
+BLANKSASNULL 
+dateformat 'mm-dd-yyyy';
+
+--tercer cargue sin delimitador, solo cargue por tamaños fijos.
+copy estudiante from '<Ruta del archivo>' 
+credentials 'aws_iam_role=<I am ARN>' 
+region '<Region>'
+fixedwidth '0:1,1:9,2:9,3:2,4:10'
+dateformat 'mm-dd-yyyy'
+;
+
+-- Tablas 
+select * from stl_load_errors order by 4 desc;
+select * from STL_LOAD_COMMITS order by 10 desc ;
+
+
