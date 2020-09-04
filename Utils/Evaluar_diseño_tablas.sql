16b000c7706abaf7a67b378791930c9fa83dabd8
Carlos Alarcon <33847175+alarcon7a@users.noreply.github.com>
2020-09-04 17:51:37 -0500

    Update Evaluar_diseño_tablas.sql

--- Utils/Evaluar_diseño_tablas.sql
+++ Utils/Evaluar_diseño_tablas.sql
@@ -1,89 +1,31 @@
-select
-	schema schemaname,
-	"table" tablename,
-	table_id tableid,
-	size size_in_mb,
-	case
-		when diststyle not in ('EVEN', 'ALL') then 1
-		else 0
-	end has_dist_key,
-	case
-		when sortkey1 is not null then 1
-		else 0
-	end has_sort_key,
-	case
-		when encoded = 'Y' then 1
-		else 0
-	end has_col_encoding,
-	cast(max_blocks_per_slice - min_blocks_per_slice as FLOAT) / greatest(NVL (min_blocks_per_slice, 0)::int, 1) ratio_skew_across_slices,
-	cast(100 * dist_slice as FLOAT) /(
-	select
-		COUNT(distinct slice)
-	from
-		stv_slices) pct_slices_populated
-from
-	svv_table_info ti
-join (
-	select
-		tbl, MIN(c) min_blocks_per_slice, MAX(c) max_blocks_per_slice, COUNT(distinct slice) dist_slice
-	from
-		(
-		select
-			b.tbl, b.slice, COUNT(*) as c
-		from
-			STV_BLOCKLIST b
-		group by
-			b.tbl, b.slice)
-	where
-		tbl in (
-		selectselect
-	schema schemaname,
-	"table" tablename,
-	table_id tableid,
-	size size_in_mb,
-	case
-		when diststyle not in ('EVEN', 'ALL') then 1
-		else 0
-	end has_dist_key,
-	case
-		when sortkey1 is not null then 1
-		else 0
-	end has_sort_key,
-	case
-		when encoded = 'Y' then 1
-		else 0
-	end has_col_encoding,
-	cast(max_blocks_per_slice - min_blocks_per_slice as FLOAT) / greatest(NVL (min_blocks_per_slice, 0)::int, 1) ratio_skew_across_slices,
-	cast(100 * dist_slice as FLOAT) /(
-	select
-		COUNT(distinct slice)
-	from
-		stv_slices) pct_slices_populated
-from
-	svv_table_info ti
-join (
-	select
-		tbl, MIN(c) min_blocks_per_slice, MAX(c) max_blocks_per_slice, COUNT(distinct slice) dist_slice
-	from
-		(
-		select
-			b.tbl, b.slice, COUNT(*) as c
-		from
-			STV_BLOCKLIST b
-		group by
-			b.tbl, b.slice)
-	where
-		tbl in (
-		select
-			table_id
-		from
-			svv_table_info)
-	group by
-		tbl) iq on
-	iq.tbl = ti.table_id;
-			table_id
-		from
-			svv_table_info)
-	group by
-		tbl) iq on
-	iq.tbl = ti.table_id;
+SELECT SCHEMA schemaname,
+       "table" tablename,
+       table_id tableid,
+       size size_in_mb,
+       CASE
+         WHEN diststyle NOT IN ('EVEN','ALL') THEN 1
+         ELSE 0
+       END has_dist_key,
+       CASE
+         WHEN sortkey1 IS NOT NULL THEN 1
+         ELSE 0
+       END has_sort_key,
+       CASE
+         WHEN encoded = 'Y' THEN 1
+         ELSE 0
+       END has_col_encoding,
+       CAST(max_blocks_per_slice - min_blocks_per_slice AS FLOAT) / GREATEST(NVL (min_blocks_per_slice,0)::int,1) ratio_skew_across_slices,
+       CAST(100*dist_slice AS FLOAT) /(SELECT COUNT(DISTINCT slice) FROM stv_slices) pct_slices_populated
+FROM svv_table_info ti
+  JOIN (SELECT tbl,
+               MIN(c) min_blocks_per_slice,
+               MAX(c) max_blocks_per_slice,
+               COUNT(DISTINCT slice) dist_slice
+        FROM (SELECT b.tbl,
+                     b.slice,
+                     COUNT(*) AS c
+              FROM STV_BLOCKLIST b
+              GROUP BY b.tbl,
+                       b.slice)
+        WHERE tbl IN (SELECT table_id FROM svv_table_info)
+        GROUP BY tbl) iq ON iq.tbl = ti.table_id;
